16a4eeca7d105ddaefb2bf6e8bcd16f81de0debe
Kunal Gupta <kunalgupta2k12@gmail.com>
2020-07-07 17:52:43 +0530

    Added skeleton for utils

--- utils.py
+++ utils.py
@@ -1,3 +1,12 @@
 """
 This module has reusable functions used throughout the application to support all the processes
 """
+
+
+class PatientUtils:
+    def registerPatient(self, name, age):
+        try:
+            # TODO: Logic to create Patient id 'xxxx' and add it to PatientRecord
+            pass
+        except Exception as e:
+            raise e
